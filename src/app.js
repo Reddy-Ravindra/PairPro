512d9a8ac374a0b8dd234ce529caab6d09cdfb7f
reddy-ravindra <54063485+Reddy-Ravindra@users.noreply.github.com>
2025-07-25 19:32:17 -0400

    Worked on routes and request handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello you are on Dashboard");
+
+app.get('/user', (req, res) => {
+  res.send({ firstname: "Ravindra", lastname: "Reddy" });
 });
 
 
 
-app.use("/profile", (req, res) => {
-    res.send("Hello, This is server running on PORT: 8080");
-});
+
 
 app.listen(8080, () => {
-    console.log("Server is running on http://localhost:8080");
-})
+  console.log("Server is running on http://localhost:8080");
+});
