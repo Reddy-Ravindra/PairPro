ce33023d36474e1860db854bbcaec49957c37e34
reddy-ravindra <54063485+Reddy-Ravindra@users.noreply.github.com>
2025-08-01 14:26:43 -0400

    Added data Sanitization and schema validation

--- src/app.js
+++ src/app.js
@@ -6,16 +6,12 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/User");
 
+app.use(express.json());
+
 
 app.post("/signup", async (req, res) => {
 
-  const user = new User({
-    firstName: "Ravindra",
-    lastName: "Reddy",
-    emailId: "info.ravindrareddy@gmail.com",
-    password: "ravindra@123",
-    age: 27,
-  })
+  const user = new User(req.body);
 
   try {
     await user.save();
@@ -25,6 +21,89 @@ app.post("/signup", async (req, res) => {
     res.status(500).send("Error adding user: " + err.message);
   }
   
+});
+
+app.get('/user', async (req, res) => {
+  const email = req.body.emailId;
+
+  await User.find({ emailId: email })
+    .then((user) => {
+      if (user.length > 0) {
+        res.send(user);
+      } else {
+        res.status(404).send("User not found");
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Error fetching user: " + err.message);
+    });
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Error fetching users: " + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const result = await User.deleteOne({ _id: userId });
+
+    if (result.deletedCount > 0) {
+      res.send("User deleted successfully");
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(500).send("Error deleting user: " + err.message);
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = [
+      "userId",
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((key) =>
+      ALLOWED_UPDATES.includes(key)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Invalid update fields provided");
+    }
+
+    if (data?.skills.length > 10) {
+      return res.status(400).send("Skills cannot exceed 10 items");
+    }
+
+    const result = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    if (result.modifiedCount > 0) {
+      res.send("User updated successfully");
+    } else {
+      res.status(404).send("User not found or no changes made");
+    }
+  } catch (err) {
+    res.status(500).send("Error updating user: " + err.message);
+  }
 });
 
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,29 +1,77 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     emailId: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: (value) => {
+        if (validator.isEmpty(value)) {
+          throw new Error("Email cannot be empty");
+        }
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid: " + value);
+        }
+        if (!validator.isLength(value, { min: 5, max: 100 })) {
+          throw new Error("Email length must be between 5 and 100 characters");
+        }
+      },
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      validate: (value) => {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password: " + value);
+        }
+      },
     },
     age: {
-        type: Number,
+      type: Number,
+      min: 16,
     },
     gender: {
-        type: String,
-    }
-});
+      type: String,
+      validate: (value) => {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.un.org/pga/wp-content/uploads/sites/53/2018/09/Dummy-image-1.jpg",
+      validate: (value) => {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "No description provided",
+    },
+    skills: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
